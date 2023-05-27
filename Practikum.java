9122254468f0770aebf82e4542ed0ca54acf5c36
Valentin <valentin.geras@yandex.ru>
2023-05-27 11:29:12 +0300

    add string out on desctop messeng

--- Practikum.java
+++ Practikum.java
@@ -6,4 +6,6 @@ class Main {
     public static void main(String[] args) {
        
         System.out.println("Сколько денег у вас осталось до зарплаты?");
+        System.out.println("Приблизительно достаточно");
+        
     }
